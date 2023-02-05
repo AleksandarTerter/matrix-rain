88de32b86aa4491d95ed7669be3ad15e3c785283
Aleksandar Todorov <eng.a.todorov@proton.me>
2023-02-05 11:04:44 +0200

    make drops fall in diferent way

--- rain.js
+++ rain.js
@@ -4,32 +4,40 @@ canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
 
 let ctx = canvas.getContext("2d");
-let y = 50;
 const chars = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ";
+const cellSizeInPx = 30;
+const columnsCount = canvas.width / cellSizeInPx;
+const rowsCount = canvas.height / cellSizeInPx;
+
+const cols = [];
+for (let col = 0; col < columnsCount; col += 1) {
+    let randomRow = getRandomInt(rowsCount);
+    cols.push(randomRow);
+}
 
 function drow() {
-    if (y > canvas.height) {
-        y = 0;
-    }
-    y += 50;
     blackTush();
-    for (let x = 0; x < canvas.width; x += 50) {
-        writeASymbol(x);
+    for (let colIndex = 0; colIndex < cols.length; colIndex += 1) {
+        if (cols[colIndex] > rowsCount) {
+            cols[colIndex] = 0;
+        }
+        cols[colIndex] += 1;
+        writeASymbol(colIndex * cellSizeInPx, cols[colIndex] * cellSizeInPx);
     }
 }
 
 function blackTush() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.15)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function writeASymbol(x) {
+function writeASymbol(x,y) {
     ctx.fillStyle = "#0F0";
-    ctx.font = "50px serif";
+    ctx.font = `${cellSizeInPx * 1.2}px serif`;
     ctx.fillText(randomMatrixSymbol(), x, y);
 }
 
-setInterval(drow, 100);
+setInterval(drow, 200);
 
 function randomMatrixSymbol() {
     const randomCharIndex = getRandomInt(chars.length);
